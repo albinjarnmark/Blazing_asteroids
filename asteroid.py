44ca9ee35601590e985f7b923fa878491e685cba
albinjarnmark <albinff@icloud.com>
2026-06-27 00:00:52 +0700

    E: Added Asteroid and AsteroidField classes for asteroid mechanics

---
+++ asteroid.py
@@ -0,0 +1,28 @@
+from circleshape import CircleShape
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+import pygame
+from logger import log_event
+import random
+
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+
+    def draw(self, screen) -> None:
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+
+    def update(self, dt: float) -> None:
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        velocity1 = self.velocity.rotate(angle)
+        velocity2 = self.velocity.rotate(-angle)
+        old_radius = self.radius
+        self.radius = old_radius - ASTEROID_MIN_RADIUS
+        Asteroid(self.position.x, self.position.y, self.radius).velocity = velocity1 * 1.2
+        Asteroid(self.position.x, self.position.y, self.radius).velocity = velocity2 * 1.2
